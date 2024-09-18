6956496be71754031e8430656909cbe969e501c0
Nurudeen <abdulmajeednurudeen48@gmail.com>
2024-09-18 01:30:12 +0000

    implement update for folders

--- src/controllers/folder-controllers.js
+++ src/controllers/folder-controllers.js
@@ -19,8 +19,7 @@ const createFolderGet = async (req, res) => {
 
 const createFolderPost = async (req, res) => {
   try {
-    console.log(req.body.name);
-    await prisma.folder.create({
+    const folder = await prisma.folder.create({
       data: {
         name: req.body.name,
       },
@@ -36,6 +35,9 @@ const createFolderPost = async (req, res) => {
 const displayFoldersGet = async (req, res) => {
   try {
     const folders = await prisma.folder.findMany();
+    if (!folders) {
+      return res.status(404).send('Folders not found');
+    }
     res.render('folders', { folders });
   } catch (error) {
     console.error('Error displaying folders', error);
@@ -43,4 +45,51 @@ const displayFoldersGet = async (req, res) => {
   }
 };
 
-module.exports = { createFolderGet, createFolderPost, displayFoldersGet };
+const updateFolderGet = async (req, res) => {
+  try {
+    const id = parseInt(req.params.id, 10);
+    const folder = await prisma.folder.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!folder) {
+      return res.status(404).send('Folder not found');
+    }
+
+    res.render('update-folder-form', { folder });
+  } catch (error) {
+    console.error('Error updating folder', error);
+    res.status(500).send('Internal Server Error');
+  }
+};
+
+const updateFolderPost = async (req, res) => {
+  try {
+    const id = parseInt(req.body.id, 10);
+    const { name } = req.body;
+
+    const updateFolder = await prisma.folder.update({
+      where: {
+        id: id,
+      },
+      data: {
+        name: name,
+      },
+    });
+
+    res.redirect('/dashboard');
+  } catch (error) {
+    console.error('Error updating folder', error);
+    res.status(500).send('Internal Server Error');
+  }
+};
+
+module.exports = {
+  createFolderGet,
+  createFolderPost,
+  displayFoldersGet,
+  updateFolderGet,
+  updateFolderPost,
+};
--- src/routes/folder-routes.js
+++ src/routes/folder-routes.js
@@ -3,8 +3,16 @@ const folderRouter = express.Router();
 const controllers = require('../controllers/folder-controllers');
 
 // Folder Routes and their handlers
+
+// Create folders
 folderRouter.get('/', controllers.createFolderGet);
 folderRouter.post('/', controllers.createFolderPost);
+
+// Read folders
 folderRouter.get('/display-folders', controllers.displayFoldersGet);
+
+// Update Folders
+folderRouter.get('/update-folder/:id', controllers.updateFolderGet);
+folderRouter.post('/update-folder/:id', controllers.updateFolderPost);
 
 module.exports = folderRouter;
